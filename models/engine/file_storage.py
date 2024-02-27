11d9675f7109cb7cd208b02f98926ba89a563f2a
yichehnc <yichenc2017@gmail.com>
2024-02-27 17:16:22 +1100

    feat: update file_storage to objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,19 +24,25 @@ class Filestorage:
         obj(object): object to be set into __objects
         """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj.to_dict()
+        self.__objects[key] = obj  # an object not the dictionary
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
+        new_dict = {}
+        for key in self.__objects:
+            new_dict[key] = self.__objects[key].to_dict()
         with open(self.__file_path, 'w', encoding="UTF-8") as file:
-            json.dump(self.__objects, file)
+            json.dump(new_dict, file)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
+        from models.base_model import BaseModel
+        reloaded_dict = {}
         try:
             with open(self.__file_path, 'r', encoding="UTF-8") as file:
-                new_dict = json.load(file)
-                for key, value in new_dict.items():
-                    self.__objects[key] = value
+                reloaded_dict = json.load(file)
         except Exception:
             pass
+        for key, obj in reloaded_dict.items():
+            self.__objects[key] = BaseModel(**dict)
+        # create an object based on the dict
