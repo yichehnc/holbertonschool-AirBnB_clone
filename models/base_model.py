aef73cee4526cff13ffa23e1a230e36db5a4ad35
yichehnc <yichenc2017@gmail.com>
2024-02-27 15:27:33 +1100

    feat: recreate an instance with the dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,18 @@ import datetime
 class BaseModel:
     """ BaseModel Class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialisation of class attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.datetime.now()
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """print string"""
