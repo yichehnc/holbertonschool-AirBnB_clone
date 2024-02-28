d8ce8e13c1a3b6ae23b799d09f0adcf91ff2b1c4
haoningng <nick.nghn@gmail.com>
2024-02-28 19:56:46 +1100

    revert: the last commit

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,13 +7,13 @@ from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
     """Test case for BaseModel Class"""
-    # def test_base_save(self):
-    #     """Test save method of BaseModel"""
-    #     my_model = BaseModel()
-    #     old_updated_at = my_model.updated_at
-    #     my_model.save()
-    #     new_updated_at = my_model.updated_at
-    #     self.assertNotEqual(old_updated_at, new_updated_at)
+    def test_base_save(self):
+        """Test save method of BaseModel"""
+        my_model = BaseModel()
+        old_updated_at = my_model.updated_at
+        my_model.save()
+        new_updated_at = my_model.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
 
     def test_to_dict(self):
         """Test to_dict method"""
