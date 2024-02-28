4402a57544189282acab4cae4bb95e4e00bc9207
haoningng <nick.nghn@gmail.com>
2024-02-28 19:12:24 +1100

    feat: add unit test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,6 +2,9 @@
 """unittest for BaseModel Class"""
 import unittest
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
+import os
 
 
 class TestBaseModel(unittest.TestCase):
@@ -14,11 +17,41 @@ class TestBaseModel(unittest.TestCase):
         new_updated_at = my_model.updated_at
         self.assertNotEqual(old_updated_at, new_updated_at)
 
+    def test_reload(self):
+        """Test reload method"""
+        my_model = BaseModel()
+        my_model.save()
+        all_objs = storage.all()
+        key = "{}.{}".format(my_model.__class__.__name__, my_model.id)
+        self.assertEqual(my_model.id, all_objs[key].__dict__['id'])
+
     def test_to_dict(self):
         """Test to_dict method"""
         my_model = BaseModel()  # obj
         my_model_json = my_model.to_dict()  # dict
         self.assertEqual(my_model.id, my_model_json["id"])
+    
+    def test_str(self):
+        """Test __str__ method"""
+        my_model = BaseModel()
+        str_format = ("[{}] ({}) {}".
+                format(self.__class__.__name__, self.id, self.__dict__))
+        self.assertEqual(type(str(my_model)), type(str_format))
+
+    def test_init(self):
+        """Test __init__ method"""
+        my_model = BaseModel()
+        self.assertNotEqual(my_model.id, "")
+        self.assertNotEqual(my_model.created_at, "")
+        self.assertNotEqual(my_model.updated_at, "")
+    
+    def test_file_exist(self):
+        """
+        Test where the file exist
+        """
+        object_1 = BaseModel()
+        object_1.save()
+        self.assertTrue(os.path.exists(FileStorage._FileStorage__file_path))
 
 
 if __name__ == "__main__":
