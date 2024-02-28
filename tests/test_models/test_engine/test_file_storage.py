3719dbe9fd05518e053a80ad1c9ea90d575a5f46
haoningng <nick.nghn@gmail.com>
2024-02-28 19:47:33 +1100

    fix: revert to old code

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,22 +3,17 @@
 import unittest
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models import storage
 import os
 
 
 class TestFileStorage(unittest.TestCase):
     """Test case for FileStorage Class"""
-    def setUp(self):
-        """
-        Setup for FileStorage
-        """
-        self.storage = FileStorage()
-
     def test_all(self):
         """Test all method"""
         my_model = BaseModel()
         my_model.save()
-        all_objs = self.storage.all()
+        all_objs = storage.all()
         key = "{}.{}".format(my_model.__class__.__name__, my_model.id)
         self.assertEqual(my_model.id, all_objs[key].__dict__['id'])
 
@@ -27,9 +22,7 @@ class TestFileStorage(unittest.TestCase):
         Test for save() method
         """
         object_1 = BaseModel()
-        self.storage.new(object_1)
-        self.storage.save()
-        # name mangling in Python.
+        object_1.save()
         self.assertTrue(os.path.exists(FileStorage._FileStorage__file_path))
 
 
