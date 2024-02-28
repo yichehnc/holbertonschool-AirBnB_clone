0c8cbdbc900e8b9658ced11a18010d440715200b
haoningng <nick.nghn@gmail.com>
2024-02-28 17:12:49 +1100

    feat: add unit test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,14 +8,10 @@ class TestUser(unittest.TestCase):
     def test_user(self):
         """Test for User Class"""
         my_user = User()
-        my_user.first_name = "Betty"
-        my_user.last_name = "Bar"
-        my_user.email = "airbnb@mail.com"
-        my_user.password = "root"
-        self.assertEqual(my_user.first_name, "Betty")
-        self.assertEqual(my_user.last_name, "Bar")
-        self.assertEqual(my_user.email, "airbnb@mail.com")
-        self.assertEqual(my_user.password, "root")
+        self.assertEqual(my_user.first_name, "")
+        self.assertEqual(my_user.last_name, "")
+        self.assertEqual(my_user.email, "")
+        self.assertEqual(my_user.password, "")
 
 
 if __name__ == "__main__":
