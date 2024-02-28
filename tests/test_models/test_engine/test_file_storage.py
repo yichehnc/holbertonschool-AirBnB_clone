c1b7560dc85fae5d971c99d9b1d2d8f0fbc9051a
haoningng <nick.nghn@gmail.com>
2024-02-28 16:48:58 +1100

    feat: add more test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,13 +9,23 @@ import os
 
 class TestFileStorage(unittest.TestCase):
     """Test case for FileStorage Class"""
-    def test_save(self):
-        """Test save method"""
+    def test_all(self):
+        """Test all method"""
         my_model = BaseModel()
         my_model.save()
         all_objs = storage.all()
         key = "{}.{}".format(my_model.__class__.__name__, my_model.id)
         self.assertEqual(my_model.id, all_objs[key].__dict__['id'])
+
+    def test_save(self):
+        """
+        Test for save() method
+        """
+        object_1 = BaseModel()
+        self.storage.new(object_1)
+        self.storage.save()
+        self.assertTrue(os.path.exists(FileStorage._FileStorage__file_path))
+
 
 
 if __name__ == "__main__":
