b28588bbe5739ba982d7470ac20702b57faee9bf
haoningng <nick.nghn@gmail.com>
2024-02-28 16:55:19 +1100

    feat: add unit test for state class

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""unittest for State Class"""
+import unittest
+from models.base_model import BaseModel
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    """Test case for State Class"""
+    def test_state(self):
+        """Test for State Class"""
+        pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
